642600d213ab8b8cb0e57443495202d7780681b5
hishamss <hishamss90@gmail.com>
2020-02-22 00:47:14 -0700

    now when teh user click on the charachters the rest of charachters disappear

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
   $(".charachter").click(function() {
-    console.log(this.id);
+    var id = this.id;
+    var score;
+    if (id === "Obi-Wan-Kenobi") {
+      score = 120;
+    } else if (id === "Luke-Skywalker") {
+      score = 100;
+    } else if (id === "Darth-Sidious") {
+      score = 150;
+    } else {
+      score = 180;
+    }
+    $(".charachter-picked").text("");
+    var val =
+      '<div class="col-md-3"><div class="card charachter border-success-min" id="' +
+      id +
+      '"><div class="card-header">' +
+      id +
+      '</div><img class="img-fluid" src="assets/images/' +
+      id +
+      '.jpg" /><div class="card-body">' +
+      score +
+      "</div></div></div>";
+    $(".charachter-picked").append(val);
   });
 });
